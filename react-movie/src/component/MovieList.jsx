0fe07599d25b77e6e9d9ed8bdaf6cbee8fedb269
jjang051 <tjjjang051@nate.com>
2026-08-11 13:45:20 +0900

    css

--- react-movie/src/component/MovieList.jsx
+++ react-movie/src/component/MovieList.jsx
@@ -1,10 +1,10 @@
 import MovieCard from "./MovieCard";
-
+import styles from "../css/MovieList.module.css";
 function MovieList({ movies }) {
-  console.log("props.movies===", movies);
+  //console.log("props.movies===", movies);
   return (
     <>
-      <section className="list">
+      <section className={styles.list}>
         {movies.map((movie, idx) => {
           console.log(idx + 1, "===", movie);
           return <MovieCard movie={movie}></MovieCard>;
